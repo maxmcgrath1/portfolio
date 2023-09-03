88f2f810a41af182a177e83207c04f635517a332
maxmcgrath1 <tuc06096@temple.edu>
2023-09-02 21:02:57 -0400

    return div

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>THIS IS WHERE THINGS GO NOW</h1>
+      <div className='projects'>
+        name: "Blackjack",
+        "live": "https://maxmcgrath1.github.io/Blackjack/",
+        "git": "https://github.com/maxmcgrath1/Blackjack",
+        "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.SRzccZ8iRnfXTRYQk0jrqQHaGu%26pid%3DApi&f=1",
+        "description": "A browser based blackjack game that utilizes HTML, CSS, and Javascript.";
+      </div>
     </div>
   );
 }
