855be34c1fe5bb288af9366212cbd48e7abeee3c
maxmcgrath1 <tuc06096@temple.edu>
2023-09-07 17:37:56 -0400

    rendering projects

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <div className="projects">
         name: "Blackjack",
-        "live": "https://maxmcgrath1.github.io/Blackjack/",
+        <button>"live": "https://maxmcgrath1.github.io/Blackjack/",</button>
         "git": "https://github.com/maxmcgrath1/Blackjack",
         "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.SRzccZ8iRnfXTRYQk0jrqQHaGu%26pid%3DApi&f=1",
         "description": "A browser based blackjack game that utilizes HTML, CSS, and Javascript.";
