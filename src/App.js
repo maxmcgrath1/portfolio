e29bc0310f01e78627bee620021f3c1d27422f7d
maxmcgrath1 <tuc06096@temple.edu>
2023-09-08 15:15:59 -0400

    blackjack

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className = "homeMessage">Hello and welcome to my portfolio!
-      </h1>
+      <h1 className = "homeMessage">Hello and welcome to my portfolio!</h1>
+      <hr />
+      <hr />
       <h1 className ="projname">Blackjack</h1>
     </div>
   );
