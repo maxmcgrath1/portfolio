82cbfc3b070765600bcbe08d8e733ccc98de280f
maxmcgrath1 <tuc06096@temple.edu>
2023-09-08 15:10:24 -0400

    welcome message

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className = "homeMessage">Welcome to my portfolio! Click on projects to see a list < br />
-        of what I have worked on so far.
-    </h1>
+      <h1 className = "homeMessage">Hello and welcome to my portfolio!
+      </h1>
     </div>
   );
 }
