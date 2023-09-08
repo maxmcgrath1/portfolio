6c662a21abc8700d0249c4daa60abe1739f5a2e4
maxmcgrath1 <tuc06096@temple.edu>
2023-09-08 15:03:46 -0400

    restructuring render div

--- src/App.js
+++ src/App.js
@@ -3,16 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="projects">
-        name: "Blackjack",
-        <button>"live": "https://maxmcgrath1.github.io/Blackjack/",</button>
-        "git": "https://github.com/maxmcgrath1/Blackjack",
-        "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.SRzccZ8iRnfXTRYQk0jrqQHaGu%26pid%3DApi&f=1",
-        "description": "A browser based blackjack game that utilizes HTML, CSS, and Javascript.";
-      </div>
-      <hr />
-      Blackjack
-      <img></img>
+      <h1 className = "homeMessage">Welcome to my portfolio! Click on projects to see a list < br />
+        of what I have worked on so far.
+    </h1>
     </div>
   );
 }
