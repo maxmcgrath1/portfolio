516914cd4c20d13be1660aae76c3f7bea70e44b9
maxmcgrath1 <tuc06096@temple.edu>
2023-09-11 18:37:42 -0400

    importing components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './components/header/Header'
 
 const App = () => {
     return (
-        <div>App</div>
+        <>
+            <Header />
+        </>
     )
 }
 
