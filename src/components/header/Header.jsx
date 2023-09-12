20f438a9a25395181456dbb431a6d7dbcaaf3ce4
maxmcgrath1 <tuc06096@temple.edu>
2023-09-12 13:48:15 -0400

    header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,13 @@ import './header.css'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Max McGrath</h1>
+                <h5 className="text-light">Fullstack Developer</h5>
+            </div>
+        </header>
     )
 }
 
