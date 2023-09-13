53df1b1521b9b0d7efdd6b1cd474fdeef8a97f78
maxmcgrath1 <tuc06096@temple.edu>
2023-09-13 13:47:52 -0400

    CTAs for header buttons

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+    return (
+        <div className='cta'>
+            <a href=''>Download DV</a>
+            <a href=''>Contact Me</a>
+        </div>
+    )
+}
+
+export default CTA
