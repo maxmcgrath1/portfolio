964508829837521f0e0fe143efdc48f0b4791e9a
maxmcgrath1 <tuc06096@temple.edu>
2023-09-13 13:15:31 -0400

    adding new createRoot method for react 18

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import App from "./App";
+import * as ReactDOMClient from 'react-dom/client'
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+// ReactDOM.render(<App/>, document.querySelector("#root"))
+
+const container = document.getElementById('root')
+const root = ReactDOMClient.createRoot(container)
+
+root.render(<App />)
