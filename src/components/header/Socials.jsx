bbde4d6cae01df6b337def01315e11ecfe4979ed
maxmcgrath1 <tuc06096@temple.edu>
2023-09-13 18:40:52 -0400

    added github icon

--- src/components/header/Socials.jsx
+++ src/components/header/Socials.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
 
 const Socials = () => {
     return (
         <div className='header__socials'>
             <a href="https://www.linkedin.com/in/max-mcgrath1/" target="_blank"><BsLinkedin/></a>
-            <a href="https://github.com/maxmcgrath1" target="_blank">GitHub</a>
+            <a href="https://github.com/maxmcgrath1" target="_blank"><BsGithub/></a>
         </div>
     )
 }
