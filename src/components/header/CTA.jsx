4bce92b3a83ee63b0d296de08a7773fc582998ae
maxmcgrath1 <tuc06096@temple.edu>
2023-09-13 15:16:28 -0400

    resume and contact are now buttons

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,8 +4,8 @@ import CV from '../../assets/resume.pdf'
 const CTA = () => {
     return (
         <div className='cta'>
-            <a href={CV} download>Download CV</a>
-            <a href='#contact'>Contact Me</a>
+            <a href={CV} download className='btn'>Download CV</a>
+            <a href='#contact'className='btn btn-primary'>Contact Me</a>
         </div>
     )
 }
