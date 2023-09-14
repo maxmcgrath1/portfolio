d25579d1a6b9da45d5ae20328ef49013a71da7a1
maxmcgrath1 <tuc06096@temple.edu>
2023-09-14 14:27:14 -0400

    removed headshot bg and adding bgtexture files

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import headshot from '../../assets/headshot1.JPG'
+import headshot from '../../assets/headshot1.png'
 import Socials from './Socials'
 
 const Header = () => {
