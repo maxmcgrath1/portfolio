d4e2fd18283538894d795092ac807ef79b77eef8
maxmcgrath1 <tuc06096@temple.edu>
2023-09-15 00:10:39 -0400

    adding nav icons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
 
 const Nav = () => {
     return (
-        <div>Nav</div>
+        <nav>
+            <a href="#"><AiOutlineHome/></a>
+            <a href="#about"><AiOutlineHome/></a>
+            <a href="#experience"><AiOutlineHome/></a>
+            <a href="#portfolio"><AiOutlineHome/></a>
+            <a href="#contact"><AiOutlineHome/></a>
+        </nav>
     )
 }
 
