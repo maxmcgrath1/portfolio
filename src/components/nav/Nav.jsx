050836638d46250804c676c03f8718dd726aac9b
maxmcgrath1 <tuc06096@temple.edu>
2023-09-15 13:31:55 -0400

    useState for active nav button

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,11 +5,13 @@ import {IoIosContact} from 'react-icons/io'
 import {BiBook} from 'react-icons/bi'
 import {BsBriefcaseFill} from 'react-icons/bs'
 import {BiSolidMessageDetail} from 'react-icons/bi'
+import { useState } from 'react'
 
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState('#')
     return (
         <nav>
-            <a href="#"><AiOutlineHome/></a>
+            <a href="#header"><AiOutlineHome/></a>
             <a href="#about"><IoIosContact/></a>
             <a href="#experience"><BiBook/></a>
             <a href="#portfolio"><BsBriefcaseFill/></a>
