78e589e0c6543d710a19c6380be085468d6a5f3c
maxmcgrath1 <tuc06096@temple.edu>
2023-09-18 13:00:26 -0400

    experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -5,6 +5,9 @@ const Experience = () => {
     return (
         <section id='experience'>
             <h5>What Skills I have</h5>
+            <h2>My Experience</h2>
+
+            
         </section>
     )
 }
