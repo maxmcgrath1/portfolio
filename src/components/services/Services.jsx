85054ab54f365648d0b94992c0678e57acc241e1
maxmcgrath1 <tuc06096@temple.edu>
2023-09-19 14:09:25 -0400

    services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -8,7 +8,17 @@ const Services = () => {
             <h2>My Services</h2>
 
             <div className="container services__container">
-                
+                <article className="service">
+                    <div className="service__head">
+                        <h3>UI/UX Design</h3>
+                    </div>
+
+                    <ul className='service__list'>
+                        <li>
+                            
+                        </li>
+                    </ul>
+                </article>
             </div>
         </section>
     )
