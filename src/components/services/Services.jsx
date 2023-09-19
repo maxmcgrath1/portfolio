19efb25b2404ef7174a2596f6ab124d1a8af7645
maxmcgrath1 <tuc06096@temple.edu>
2023-09-19 13:59:43 -0400

    services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,7 +3,14 @@ import './services.css'
 
 const Services = () => {
     return (
-        <section id='services'>Services</section>
+        <section id='services'>
+            <h5>What I Offer</h5>
+            <h2>My Services</h2>
+
+            <div className="container services__container">
+                
+            </div>
+        </section>
     )
 }
 
