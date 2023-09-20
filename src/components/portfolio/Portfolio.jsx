6347411f0fab9abd1e572a5e20edf04009ec7a72
maxmcgrath1 <tuc06096@temple.edu>
2023-09-20 13:55:01 -0400

    added rel noreferrer to get rid of warnings

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -16,9 +16,33 @@ const Portfolio = () => {
                     <div className="portfolio__item-image">
                         <img src={blackjackLogo} alt="Blackjack" />
                     </div>
-                    <h3>This is a portfolio item title</h3>
-                    <a href="https://github.com/maxmcgrath1/Blackjack" className='btn' target='_blank'>GitHub</a>
-                    <a href="https://maxmcgrath1.github.io/Blackjack/" className='btn' target='_blank'>Live Site</a>
+                    <h3>Blackjack</h3>
+                    <a href="https://github.com/maxmcgrath1/Blackjack" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+                    <a href="https://maxmcgrath1.github.io/Blackjack/" className='btn' target='_blank' rel="noreferrer">Live Site</a>
+                </article>
+                <article className='portfolio__item'>
+                    <div className="portfolio__item-image">
+                        <img src={ggLogo} alt="Blackjack" />
+                    </div>
+                    <h3>Good Games</h3>
+                    <a href="https://github.com/maxmcgrath1/Blackjack" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+                    <a href="https://maxmcgrath1.github.io/Blackjack/" className='btn' target='_blank' rel="noreferrer">Live Site</a>
+                </article>
+                <article className='portfolio__item'>
+                    <div className="portfolio__item-image">
+                        <img src={fumblrLogo} alt="Blackjack" />
+                    </div>
+                    <h3>fumblr</h3>
+                    <a href="https://github.com/maxmcgrath1/Blackjack" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+                    <a href="https://maxmcgrath1.github.io/Blackjack/" className='btn' target='_blank' rel="noreferrer">Live Site</a>
+                </article>
+                <article className='portfolio__item'>
+                    <div className="portfolio__item-image">
+                        <img src={wayfarerLogo} alt="Blackjack" />
+                    </div>
+                    <h3>Wayfarer</h3>
+                    <a href="https://github.com/maxmcgrath1/Blackjack" className='btn' target='_blank' rel="noreferrer">GitHub</a>
+                    <a href="https://maxmcgrath1.github.io/Blackjack/" className='btn' target='_blank' rel="noreferrer">Live Site</a>
                 </article>
             </div>
         </section>
