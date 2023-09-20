cf94f1b0036da90cb472375696f6c730afbecbbc
maxmcgrath1 <tuc06096@temple.edu>
2023-09-20 13:17:36 -0400

    portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,18 @@ import './portfolio.css'
 
 const Portfolio = () => {
     return (
-        <section id='portfolio'>Portfolio</section>
+        <section id='portfolio'>
+            <h5>My Recent Work</h5>
+            <h2>My Portfolio</h2>
+
+            <div className="container portfolio__container">
+                <article className='portfolio__item'>
+                    <div className="portfolio__item-image">
+                        <h3>This is a portfolio item title</h3>
+                    </div>
+                </article>
+            </div>
+        </section>
     )
 }
 
