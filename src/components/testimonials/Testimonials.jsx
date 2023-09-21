f373691d8a174a3c322baf37a9fd19e34ec99611
maxmcgrath1 <tuc06096@temple.edu>
2023-09-21 12:42:54 -0400

    testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './testimonials.css'
+import avatar from '../../assets/headshot1.png'
 
 const Testimonials = () => {
     return (
-        <section id='testimonials'>Testimonials</section>
+        <section id='testimonials'>
+            <h5>Reviews from Clients</h5>
+            <h2>Testimonials</h2>
+
+            <div className="container testimonials__container">
+                <article className='testimonial'>
+                    <div className="client__avatar">
+                        <img src={avatar} alt="" />
+                    </div>
+                </article>
+            </div>
+        </section>
     )
 }
 
