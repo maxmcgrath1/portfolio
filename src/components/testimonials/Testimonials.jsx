0181f2f19680cfa850f58cd6146d597ea305a433
maxmcgrath1 <tuc06096@temple.edu>
2023-09-21 14:41:10 -0400

    adding swiper for carousel

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import './testimonials.css'
 import AVTR from '../../assets/headshot1.png'
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
 const data = [
     {
         avatar: AVTR,
@@ -31,23 +41,23 @@ const Testimonials = () => {
             <h5>Reviews from Clients</h5>
             <h2>Testimonials</h2>
 
-            <div className="container testimonials__container">
+            <Swiper className="container testimonials__container">
                 {
                     data.map(({avatar, name, review}, index) => {
                         return (
-                            <article key={index} className='testimonial'>
-                            <div className="client__avatar">
-                                <img src={avatar} alt="Client Avatar" />
-                            </div>
-                            <h5 className='client__name'>{name}</h5>
-                            <small className='client__review'>
-                                {review}
-                            </small>
-                        </article>
+                            <SwiperSlide key={index} className='testimonial'>
+                                <div className="client__avatar">
+                                    <img src={avatar} alt="Client Avatar" />
+                                </div>
+                                <h5 className='client__name'>{name}</h5>
+                                <small className='client__review'>
+                                    {review}
+                                </small>
+                            </SwiperSlide>
                         )
                     })
                 }
-            </div>
+            </Swiper>
         </section>
     )
 }
