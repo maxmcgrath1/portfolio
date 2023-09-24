9cdfb66d2bedf1e8e1ab5840fe11e2c52ffb619f
maxmcgrath1 <tuc06096@temple.edu>
2023-09-23 21:06:00 -0400

    exp rewording

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -5,7 +5,7 @@ import {BsFillPatchCheckFill} from 'react-icons/bs'
 const Experience = () => {
     return (
         <section id='experience'>
-            <h5></h5>
+            <h5>My Experience</h5>
             <h2></h2>
 
             <div className='container experience__container'>
