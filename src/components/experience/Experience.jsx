dd8837935dd992a78099434a9fc9020275bed72d
maxmcgrath1 <tuc06096@temple.edu>
2023-09-23 21:05:38 -0400

    exp rewording

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -6,7 +6,7 @@ const Experience = () => {
     return (
         <section id='experience'>
             <h5>What Skills I have</h5>
-            <h2>My Experience</h2>
+            <h2></h2>
 
             <div className='container experience__container'>
                 <div className='experience__frontend'>
