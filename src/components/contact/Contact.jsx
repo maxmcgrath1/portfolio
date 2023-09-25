2e6bd9656ec7595ef0d4c444424d7438ca241d1d
maxmcgrath1 <tuc06096@temple.edu>
2023-09-25 13:24:34 -0400

    adding message form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -36,6 +36,7 @@ const Contact = () => {
                     <input type="text" name="name" placeholder="Your Name" required />
                     <input type="email" name="email" placeholder="Your Email" required />
                     <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+                    <button type="submit">Send Message</button>
                 </form>
             </div>
         </section>
