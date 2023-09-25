e0af13b2188f964e469022b573224b0a836f612d
maxmcgrath1 <tuc06096@temple.edu>
2023-09-25 12:59:44 -0400

    contact messenger

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './contact.css'
 import {MdOutlineMail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
 
 const Contact = () => {
     return (
@@ -10,6 +11,18 @@ const Contact = () => {
 
             <div className="container contact__container">
                 <div className="contact__options">
+                    <article className="contact__options">
+                        <MdOutlineMail />
+                        <h4>Email</h4>
+                        <h5>maxmcgrath@temple.edu</h5>
+                        <a href="mailto:maxmcgrath@temple.edu">Email Me</a>
+                    </article>
+                    <article className="contact__options">
+                        <RiMessengerLine />
+                        <h4>Messenger</h4>
+                        <h5>Max McGrath</h5>
+                        <a href="https://m.me/MaxWellAmillionImus">Email Me</a>
+                    </article>
                     <article className="contact__options">
                         <MdOutlineMail />
                         <h4>Email</h4>
