6204dd28341f01be563befdc15f75113889d08bd
maxmcgrath1 <tuc06096@temple.edu>
2023-09-26 17:18:25 -0400

    footer css

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
 
     return (
         <section id='contact'>
-            <h5>Get In Tocuh</h5>
+            <h5>Get In Touch</h5>
             <h2>Contact Me</h2>
 
             <div className="container contact__container">
