4fd185caf4c91875c69b6cf96ed0960d2477e592
maxmcgrath1 <tuc06096@temple.edu>
2023-09-29 20:50:19 -0400

    exp

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -85,7 +85,7 @@ const Experience = () => {
                             <BsFillPatchCheckFill className='experience__details-icon' />
                             <div>
                                 <h4>PostgreSQL</h4>
-                                <small className='text-light'>Experienced</small>
+                                <small className='text-light'>Intermediate</small>
                             </div>
                         </article>
                         <article className='experience__details'>
